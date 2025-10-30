c916aa82ede3bc5f7ff608e4961bdf2c667b605d
Velazquez <velazqud@legal.regn.net>
2025-10-29 22:51:36 -0600

    Fix: corregir error de sintaxis en footer.tsx - cerrar etiqueta span correctamente

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -75,7 +75,7 @@ export function Footer() {
               </li>
               <li className="flex items-center space-x-2">
                 <MapPin className="h-4 w-4" />
-                <span>Mérida, Yucatán/span>
+                <span>Mérida, Yucatán</span>
               </li>
             </ul>
           </div>
